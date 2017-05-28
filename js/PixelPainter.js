6612b55c79f505bba1aaf5921a731f85c5824e9d
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2017-05-27 18:39:59 -1000

    reorganized divs; functions more dynamic

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,21 +1,23 @@
 function pixelPainter(){
+  var ppcolors = document.querySelector("#ppcolors");
+  var ppcanvas = document.querySelector("#ppcanvas");
   var saveColor = "";
   var colorArr = ["red", "blue", "yellow", "green", "black", "orange", "purple"];
 
 
-  function makePixel(){
+  function makePixel(elClassName, target){
     var pixel = document.createElement("div");
-    pixel.className = "pixel";
+    pixel.className = elClassName;
     pixel.addEventListener("click", function(){
       this.style.backgroundColor = "black";
     });
-    ppcanvas.appendChild(pixel);
+    target.appendChild(pixel);
   }
 
   function makeCanvas(w, h){
     var num = w * h;
     for (var i = 0; i < num; i++){
-      makePixel();
+      makePixel("pixel", ppcanvas);
       if (i % w === w-1){
         ppcanvas.appendChild(document.createElement("br"));
       }
@@ -23,40 +25,47 @@ function pixelPainter(){
     }
   }
 
-  function colorSwatch(){
-    var colorSet = document.createElement("div");
-    colorSet.className = "colors";
-    var colorCanvas = makeCanvas(2, 5);
-    colorSet.appendChild(colorCanvas);
+  function makeColorSwatch(w, h){
+    // var colorSet = document.querySelector("#ppcolors");
+    // ppcolors.appendChild(colorCanvas);
+    var num = w * h;
+    for (var i = 0; i < num; i++){
+      makePixel("colors", ppcolors);
+      if (i % w === w-1){
+        ppcolors.appendChild(document.createElement("br"));
+      }
+
+    }
 
   }
 
   function setColor(color) {
     this.style.backgroundColor = color;
   }
+  // var colorCanvas = makeCanvas();
+  makeColorSwatch(2, 10);
+  makeCanvas(10, 10);
 
-  makeCanvas(30, 30);
 
+  // function clearCanvas() {
+  // var blkOut = document.querySelectorAll(".pixel");
+  // blkOut.style.backgroundColor = "black";
+  // console.log(blkOut);
+  // }
 
-  function clearCanvas() {
-  var blkOut = document.querySelectorAll(".pixel");
-  blkOut.style.backgroundColor = "black";
-  console.log(blkOut);
-  }
-
-  var clear = document.createElement("button");
-  clear.innerHTML = "clear";
-  clear.addEventListener("click", function(){
-    clearCanvas();
-  });
-  ppcanvas.appendChild(clear);
+  // var clear = document.createElement("button");
+  // clear.innerHTML = "clear";
+  // clear.addEventListener("click", function(){
+  //   clearCanvas();
+  // });
+  // ppcanvas.appendChild(clear);
 
   return {
 
   makePixel : makePixel,
   makeCanvas : makeCanvas,
-  setColor : setColor,
-  colorSwatch : colorSwatch
+  // setColor : setColor,
+  makeColorSwatch : makeColorSwatch
 
   };
 }
