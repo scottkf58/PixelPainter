bb904167c83cf67a8b6c409e3e49d419d756844f
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2017-05-31 17:57:00 -1000

    added comments; changed location of buttons

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,22 +1,21 @@
 function pixelPainter(){
-  var ppcolors = document.querySelector("#ppcolors");
-  var ppcanvas = document.querySelector("#ppcanvas");
   var preColor;
   var currColor;
   var coloredPix = [];
+  var ppcolors = document.querySelector("#ppcolors");
+  var ppcanvas = document.querySelector("#ppcanvas");
   var colorArr = ["red", "blue", "yellow", "green", "black", "orange", "purple", "brown", "gray", "salmon"];
 
+  // Generates each pixel
   function makePixel(elClassName, target){
     var pixel = document.createElement("div");
     pixel.className = elClassName;
-    // pixel.addEventListener("click", function(){
-    //   this.style.backgroundColor = "black";
-    // });
     target.appendChild(pixel);
     coloredPix.push();
     return pixel;
   }
 
+// Creates a canvas w/ w * h pixels
   function makeCanvas(w, h){
     var num = w * h;
     for (var i = 0; i < num; i++){
@@ -25,13 +24,11 @@ function pixelPainter(){
         ppcanvas.appendChild(document.createElement("br"));
       }
       pixel.addEventListener("click", paintColor);
-
     }
   }
 
+// Creates a color palette of colors to choose from
   function makeColorSwatch(w, h){
-    // var colorSet = document.querySelector("#ppcolors");
-    // ppcolors.appendChild(colorCanvas);
     var num = w * h;
     for (var i = 0; i < num; i++){
       var pixel = makePixel("colors", ppcolors);
@@ -43,43 +40,50 @@ function pixelPainter(){
     }
   }
 
+// Saves the selected color from color palette
   function setColor(){
     preColor = this.style.backgroundColor;
     currColor = preColor;
     console.log(currColor);
   }
 
+// Paints pixel
  function paintColor(){
     this.style.backgroundColor = currColor;
     console.log(currColor);
   }
 
-  makeColorSwatch(2, 5);
+  makeColorSwatch(5, 2);
   makeCanvas(30, 30);
 
+// Creates button to clear the entire canvas
+var clear = document.createElement("button");
+  clear.id = "clear";
+  clear.innerHTML = "clear";
+  clear.addEventListener("click", clearCanvas);
+  ppcolors.appendChild(clear);
+
+// Clears the entire canvas
   function clearCanvas() {
     var pixels = document.querySelectorAll(".pixel");
     for (var i = 0; i < pixels.length; i++){
       var pixel = pixels[i];
       if (pixel.style.backgroundColor !== ""){
        pixel.style.backgroundColor = "";
+      }
     }
   }
-  }
 
-  var clear = document.createElement("button");
-  clear.innerHTML = "clear";
-  clear.addEventListener("click", clearCanvas);
-  ppcanvas.appendChild(clear);
-
+// Creates button to erase selected pixel
   var erase = document.createElement("button");
+  erase.id = "erase";
   erase.innerHTML = "erase";
   erase.addEventListener("click", function(){
       currColor = "";
       this.style.backgroundColor = "";
       console.log("erase");
     });
-  ppcanvas.appendChild(erase);
+  ppcolors.appendChild(erase);
 
   return {
 
@@ -93,7 +97,5 @@ function pixelPainter(){
 }
 
 var paint = pixelPainter();
-
-
 
 
